128018ed03600d075c7366899a4639aeccebb827
neeliharshitha <vtu26956@veltech.edu.in>
2026-07-26 22:00:06 +0530

    Create program2.java

---
+++ javabasicprograms/program2.java
@@ -0,0 +1,16 @@
+public class Demo {
+
+    // Function declaration
+    void displayMessage() {
+        System.out.println("Welcome to Java Functions");
+    }
+
+    public static void main(String[] args) {
+
+        // Creating object
+        Demo obj = new Demo();
+
+        // Calling the function
+        obj.displayMessage();
+    }
+}
